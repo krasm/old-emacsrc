9dba46d5019d27498778a58431ecdcc199c2ca98
Marcin Krasowski <mkrasowski@gmail.com>
2013-04-19 00:10:07 +0100

    Merged settings from the other machine.

---
+++ emacs-rc-js.el
@@ -0,0 +1,13 @@
+;; java script mode
+(autoload 'js-mode "js" "Start js-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+			     "python -mjson.tool" (current-buffer) t)))
+
+;;(define-key json-mode-map (kbd "C-c C-f") 'beautify-json)
