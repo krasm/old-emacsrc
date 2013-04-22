7846726c854b85b29fff3bf84f1801b6b02e1329
Marcin Krasowski <mkrasowski@gmail.com>
2013-04-22 23:18:42 +0100

    Changes to java stuff.

--- emacs-rc-cedet.el
+++ emacs-rc-cedet.el
@@ -175,3 +175,16 @@
  '(cedet-java-jdk-root (getenv "JAVA_HOME"))
  '(semanticdb-javap-classpath (concat (getenv "JAVA_HOME") "/jre/lib/rt.jar"))
 )
+
+(require 'flymake)
+(add-hook 'java-mode-hook 'flymake-mode-on)
+
+(defun my-java-flymake-init ()
+  (list "javac" (list (flymake-init-create-temp-buffer-copy
+		       'flymake-create-temp-with-folder-structure))))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.java$" my-java-flymake-init flymake-simple-cleanup))
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
