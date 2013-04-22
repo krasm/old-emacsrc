8efc8c81a548034cd3d43f2937bdf8d2ee8f6677
krasm <mkrasowski@gmail.com>
2013-04-22 22:04:52 +0100

    Added cedet

--- init.el
+++ init.el
@@ -24,3 +24,24 @@
 
 ;; java stuff
 (load-library "emacs-rc-java")
+
+;; cedet 
+(load-library "emacs-rc-cedet")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ede-project-directories (quote ("/home/a521286/erc/trunk")))
+ '(nxml-attribute-indent 2)
+ '(nxml-auto-insert-xml-declaration-flag t)
+ '(nxml-bind-meta-tab-to-complete-flag t)
+ '(nxml-slash-auto-complete-flag t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
